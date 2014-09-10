6071b44a05a76b1312a80733c462c775c52ace8f
Brandy C. Lucero <brandy.c.lucero@gmail.com>
2014-09-10 11:52:21 -0600

    finished 9/10/2014

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -13,24 +13,73 @@ namespace TriviaGame
         {
             //The logic for your trivia game happens here
             List<Trivia> AllQuestions = GetTriviaList();
-        }
 
+            Greeting();
 
+             //delare variables for questions that are given
+            int score = 0;
+            int correct = 0;
+            int wrong = 0;
+
+             
+            //while loop if score is greater than 0 play
+            while (wrong < 10)
+            {
+                 //create a random generator for trivia quesions
+                Random rng = new Random();
+                int newRandom = rng.Next(1,5001);
+
+                var triviaQuestion = GetTriviaList()[newRandom];
+
+                Console.WriteLine("Your answer is: ");
+                string input = Console.ReadLine();
+                
+                if (input == triviaQuestion.Answer.ToLower())
+                {
+                    correct++; score += 100;
+                    Console.Clear();
+                    Console.WriteLine("You are awesomeness!");
+                }
+                        else   
+                {
+                     wrong++;
+                    score -= 50;
+                    Console.Clear();
+                    Console.WriteLine(input + " is wrong!\nThe correct answer is: " + triviaQuestion.Answer + "\n");
+                }
+                Console.WriteLine("Correct: " + correct + "\nScore: " + score + "\nYou have " + (10 - wrong) + " incorrect guesses remaining.\n");
+            }
+            Console.ReadKey();
+            }
         //This functions gets the full list of trivia questions from the Trivia.txt document
         static List<Trivia> GetTriviaList()
         {
             //Get Contents from the file.  Remove the special char "\r".  Split on each line.  Convert to a list.
             List<string> contents = File.ReadAllText("trivia.txt").Replace("\r", "").Split('\n').ToList();
 
-            //Each item in list "contents" is now one line of the Trivia.txt document.
-            
             //make a new list to return all trivia questions
             List<Trivia> returnList = new List<Trivia>();
             // TODO: go through each line in contents of the trivia file and make a trivia object.
             //       add it to our return list.
             // Example: Trivia newTrivia = new Trivia("what is my name?*question");
+            
+            foreach (string s in contents)
+            {
+                //Declare newTrivia and split into question and answer
+                Trivia newTrivia = new Trivia(s);
+                returnList.Add(newTrivia);
+            }
             //Return the full list of trivia questions
             return returnList;
         }
+
+        static void Greeting()
+        {
+            Console.Write("Please enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Hello, " + name + " you will answer questions until you get 10 wrong or hit Ctrl+C.");
+        }
     }
 }
+
+     
--- TriviaGame/Trivia.cs
+++ TriviaGame/Trivia.cs
@@ -9,15 +9,27 @@ namespace TriviaGame
     class Trivia
     {
         //TODO: Fill out the Trivia Object
-        
         //The Trivia Object will have 2 properties
-        // at a minimum, Question and Answer
+        //create properties for trivia game
+        public string Question { get; set; }
+        public string Answer { get; set; }
 
+        // at a minimum, Question and Answer
         //The Constructor for the Trivia object will
         // accept only 1 string parameter.  You will
         // split the question from the answer in your
         // constructor and assign them to the Question
-        // and Answer properties
+        // and Answer properties.
+
+        public Trivia(string answers)
+        {
+            List<string> split = new List<string>();
+            split = answers.Split('*').ToList();
+
+            this.Question = split[0];
+            this.Answer = split[1];
+
+        }
 
 
     }
