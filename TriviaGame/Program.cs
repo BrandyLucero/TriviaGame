a348a5bd7416b78732dfa1dcfd4e0080099be19a
Brandy C. Lucero <brandy.c.lucero@gmail.com>
2014-10-05 20:48:12 -0600

    Finished

--- TriviaGame/Program.cs
+++ TriviaGame/Program.cs
@@ -13,122 +13,125 @@ namespace TriviaGame
         {
             //The logic for your trivia game happens here
             List<Trivia> AllQuestions = GetTriviaList();
+            Console.WriteLine(@"Trivia Game");
+            //ask the user for name
+            Console.WriteLine("\nWhat is your name?\n\n");
 
-            Greeting();
-
-             //delare variables for questions that are given
-            int score = 0;
-            int correct = 0;
-            int wrong = 0;
-
-             
-            //while loop if score is greater than 0 play
-            while (wrong < 10)
-            {
-                 //create a random generator for trivia quesions
-                Random rng = new Random();
-                int newRandom = rng.Next(1,5001);
-
-                var triviaQuestion = GetTriviaList()[newRandom];
-
-                Console.WriteLine("Your answer is: ");
-                string input = Console.ReadLine();
-                
-                if (input == triviaQuestion.Answer.ToLower())
-                {
-                    correct++; score += 100;
-                    Console.Clear();
-                    Console.WriteLine("You are awesomeness!");
-                }
-                        else   
-                {
-                     wrong++;
-                    score -= 50;
-                    Console.Clear();
-                    Console.WriteLine(input + " is wrong!\nThe correct answer is: " + triviaQuestion.Answer + "\n");
-                }
-                Console.WriteLine("Correct: " + correct + "\nScore: " + score + "\nYou have " + (10 - wrong) + " incorrect guesses remaining.\n");
-            }
+            //set name
+            string name = Console.ReadLine();
+            Console.Clear();
+            //greet the user and explain the trivia game!
+            Console.WriteLine(@"Hello there, " + name + "! Welcome to the Trivia Game 5000!\n\nThis is a game where I will ask you twenty trivia questions at which you will have to provide the correct answer.  You can only answer each question one time and I will let you know if you have answered correctly or incorrectly.\n\nEach question answered correctly will earn you 1000 points. If a question is answered incorrectly, no points will be deducted.  Try and answer all twenty questions correctly for a total of 20,000 points!\n\nAt the end of the game, your final stats will be displayed.\n\nGood luck!\n\n\n");
+            Console.WriteLine("Press any key to continue...");
             Console.ReadKey();
+            Console.Clear();
+            //create some variable for correct/incorrect answers, number of questions, and user's score
+            int correct = 0;
+            int incorrect = 0;
+            int questionAsk = 20;
+            int userScore = 0;
+            //create varible for question number
+            int number = 1;
+            //create a loop to run the twenty questions counting down from 20
+            while (questionAsk > 0)
+            {
+                //create a random generator to draw a question from the txt file at random
+                Random rand = new Random();
+                int randNum = rand.Next(1, 5001);
+                var triviaNum = GetTriviaList()[randNum];
+
+                Console.WriteLine("Question #" + number + ":\n\n");
+                //ask the user the random question
+                //link the Question from the trivia.cs file
+                Console.WriteLine(triviaNum.Question);
+                //get the users answer
+                Console.WriteLine("\n\nYour Answer: \n\n");
+                //convert the users answer to lowercase 
+                var userAnswer = Console.ReadLine().ToLower();
+                //see if the users answer matches up with the correct answer in our txt file
+                //link Answer from the triva.cs file
+                if (userAnswer == triviaNum.Answer.ToLower())
+                {
+                    //take one away from questions asked
+                    questionAsk = questionAsk - 1;
+                    //add 5 points to the users score
+                    userScore = userScore + 1000;
+                    //add one to correct answers
+                    correct++;
+                    //let user know that they answered correctly
+                    Console.WriteLine("\n==========Correct==========\n\n");
+                    //let user know they add 1000 pts to their score
+                    Console.WriteLine("You have earned 1000 pts!");
+                    //go on to the next question
+                    number++;
+                    Console.WriteLine("\n\n\n\nPress any key to continue...");
+                    Console.ReadKey();
+                }
+                //if answer was incorrect...
+                else
+                {
+                    //take one away from questions asked
+                    questionAsk = questionAsk - 1;
+                    //add one to incorrect answers
+                    incorrect++;
+                    //let user know they answered incorrectly
+                    Console.WriteLine("\n==========Incorrect==========\n\n");
+                    //go on to the next question
+                    number++;
+                    //tell the user the correct answer
+                    Console.WriteLine("The correct answer was: \n\n" + triviaNum.Answer);
+                    Console.WriteLine("\n\n\n\nPress any key to continue...");
+                    Console.ReadKey();
+
+                }
+                //clear console after each question
+                Console.Clear();
             }
+            //at the end of twenty questions print out to the user their score and stats
+            Console.WriteLine("Congratulations! You have finished answering all of the questions!  Here are your results:\n\n");
+            Console.WriteLine("================Game Stats================\n\n");
+            Console.WriteLine("Total Answered Correctly: " + correct);
+            Console.WriteLine("Total Answered Incorrectly: " + incorrect);
+            Console.WriteLine("Score: " + userScore + "pts");
+            Console.WriteLine("\n\n\n\nPress any key to continue...");
+            Console.ReadKey();
+            Console.Clear();
+      
+            //thank the user for playing
+            Console.WriteLine("\n\n\nThank you for playing Trivia Game 5000!");
+            Console.WriteLine("\n\nPress any key to exit.");
+
+            //keep console open
+            Console.ReadKey();
+        }
+
+
         //This functions gets the full list of trivia questions from the Trivia.txt document
         static List<Trivia> GetTriviaList()
         {
             //Get Contents from the file.  Remove the special char "\r".  Split on each line.  Convert to a list.
             List<string> contents = File.ReadAllText("trivia.txt").Replace("\r", "").Split('\n').ToList();
 
+            //Each item in list "contents" is now one line of the Trivia.txt document.
+
             //make a new list to return all trivia questions
             List<Trivia> returnList = new List<Trivia>();
             // TODO: go through each line in contents of the trivia file and make a trivia object.
             //       add it to our return list.
             // Example: Trivia newTrivia = new Trivia("what is my name?*question");
-            
-            foreach (string s in contents)
+            foreach (string question in contents)
             {
-                //Declare newTrivia and split into question and answer
-                Trivia newTrivia = new Trivia(s);
-                returnList.Add(newTrivia);
+                Trivia triviaObject = new Trivia(question);
+                //add the new object to returnList
+                returnList.Add(triviaObject);
             }
             //Return the full list of trivia questions
             return returnList;
         }
-
-        static void Greeting()
-
-        {
-            Console.Write("Please enter your name: ");
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello, " + name + " you will answer questions until you get 10 wrong or hit Ctrl+C.");
-        }
-
-        static void AddHighScore(int PlayerScore)
-        {
-            Console.WriteLine("Your Name");
-            string playerName = Console.ReadLine();
-            NORTHWNDEntities db = new NORTHWNDEntities();
-
-            //create a new highscore
-            HighScore newHighScore = new HighScore();
-            newHighScore.Date = DateTime.Now;
-            newHighScore.Game = "Trivia";
-            newHighScore.Name = playerName;
-            newHighScore.Score = PlayerScore;
-
-            //add it to the data base
-
-            db.HighScores.Add(newHighScore);
-
-            //save changes 
-            db.SaveChanges();
-            
-
-        }
-
-
-
-        static void DisplayHighScore()
-        {
-            Console.WriteLine("Display Trivia High Score");
-            Console.WriteLine("--------------");
-
-            NORTHWNDEntities db = new NORTHWNDEntities();
-            List<HighScore> highScoreList = db.HighScores.Where(x => x.Game == "Trivia")
-                .OrderByDescending(x => x.Score).Take(10).ToList();
-
-            foreach(HighScore highScore in highScoreList)
-            {
-                Console.WriteLine("{0}. {1}- {2} on {3}",
-                highScoreList.IndexOf(highScore) + 1,
-                highScore.Name,
-                highScore.Score,
-                highScore.Date.Value.ToShortDateString());
-
-            }
-        }
     }
 }
 
-        
 
+      
 
      
--- TriviaGame/Trivia.cs
+++ TriviaGame/Trivia.cs
@@ -9,28 +9,34 @@ namespace TriviaGame
     class Trivia
     {
         //TODO: Fill out the Trivia Object
+
         //The Trivia Object will have 2 properties
-        //create properties for trivia game
+        // at a minimum, Question and Answer
+
         public string Question { get; set; }
+
         public string Answer { get; set; }
 
-        // at a minimum, Question and Answer
         //The Constructor for the Trivia object will
         // accept only 1 string parameter.  You will
         // split the question from the answer in your
         // constructor and assign them to the Question
-        // and Answer properties.
-
-        public Trivia(string answers)
+        // and Answer properties
+        public Trivia(string trivialSplit)
         {
-            List<string> split = new List<string>();
-            split = answers.Split('*').ToList();
+            //create a list for the questions/answers to go to
+            List<string> QandA = new List<string>();
+            //split each question and answer from the txt file at the asteric and add to QandA list
+            QandA = trivialSplit.Split('*').ToList();
 
-            this.Question = split[0];
-            this.Answer = split[1];
+            //set up that in QandA, the question is before the * and the answer is after
+            //question would be index 0 and answer would be index 1
+            //set the values for Question and Answer
+            this.Question = QandA[0];
+            this.Answer = QandA[1];
+
 
         }
-
 
     }
 }
